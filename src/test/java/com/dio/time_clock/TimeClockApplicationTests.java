1d59576ed31a90d4e0855c5ff8d7b17085b3a9aa
ebolzan <ebolzan@inf.ufsm.br>
2022-01-22 22:50:35 -0300

    build: create base project spring

---
+++ src/test/java/com/dio/time_clock/TimeClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.time_clock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
