73143253d241e94256b0f064f64ad7052b43735f
ebolzan <ebolzan@inf.ufsm.br>
2022-01-23 10:15:00 -0300

    feat(ocorrencia): create class Ocorrencia

---
+++ src/main/java/com/dio/time_clock/model/Ocorrencia.java
@@ -0,0 +1,15 @@
+package com.dio.time_clock.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Ocorrencia {
+    private long id;
+    private String nome;
+    private String descricao;
+}
