82f61c2781e749d6f435a0442eebb570787da88f
ebolzan <ebolzan@inf.ufsm.br>
2022-01-23 10:35:00 -0300

    feat(calendario): create class Calendario

---
+++ src/main/java/com/dio/time_clock/model/Calendario.java
@@ -0,0 +1,18 @@
+package com.dio.time_clock.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Calendario {
+    private Long id;
+    private TipoData tipoData;
+    private String descricao;
+    private LocalDateTime dataEspecial;
+}
