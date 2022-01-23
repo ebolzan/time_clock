8a9c50b80d76a95f76552e6bad2c888fce6112df
ebolzan <ebolzan@inf.ufsm.br>
2022-01-23 10:11:17 -0300

    refactor(usuario): joining class relationship

--- src/main/java/com/dio/time_clock/model/Usuario.java
+++ src/main/java/com/dio/time_clock/model/Usuario.java
@@ -2,7 +2,9 @@ package com.dio.time_clock.model;
 
 
 import lombok.*;
+import org.hibernate.annotations.ManyToAny;
 
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -14,10 +16,14 @@ import java.time.LocalDateTime;
 @Builder
 public class Usuario {
     private Long id;
+    @ManyToOne
     private CategoriaUsuario categoriaUsuario;
     private String nome;
+    @ManyToOne
     private Empresa empresa;
+    @ManyToOne
     private NivelAcesso nivelAcesso;
+    @ManyToOne
     private JornadaTrabalho jornadaTrabalho;
     private BigDecimal tolerancia;
     private LocalDateTime inicioJornada;
